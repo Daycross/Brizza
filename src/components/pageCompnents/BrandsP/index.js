aa3f68086e9256b9e9111e1dceea9d2240ecdad5
Lucas  Sapucaia de Lima <lucas.lima@timeguilu.com.br>
2020-12-06 00:49:30 -0300

    Slideshow On

--- src/components/pageCompnents/BrandsP/index.js
+++ src/components/pageCompnents/BrandsP/index.js
@@ -1,9 +1,65 @@
 import React from 'react';
+import Slider from 'infinite-react-carousel';
 
 import { Container } from './styles';
 
+import Avon from '../../../assets/logosImages/avon.png';
+import BrasilCt from '../../../assets/logosImages/brasilct.png';
+import Goldratt from '../../../assets/logosImages/goldratt.png';
+import Hcentrix from '../../../assets/logosImages/hcentrix.png';
+import Kopenhagen from '../../../assets/logosImages/kopenhagen.png';
+import LaPercussi from '../../../assets/logosImages/laPercussi.png';
+import Luandre from '../../../assets/logosImages/luandre.png';
+import MachadoMeyer from '../../../assets/logosImages/machadoMeyer.png';
+import Modeat from '../../../assets/logosImages/modear.png';
+
 function LandingPage() {
-  return <Container />;
+  return (
+    <Container>
+      <p>marcas que trabalhamos</p>
+      <Slider
+        className="slideShow"
+        style={{ overflow: 'visible' }}
+        slidesPerRow={3}
+        duration={500}
+        autoplay
+        autoplaySpeed={5000}
+        adaptiveHeight
+      >
+        <div className="slide">
+          <img src={Avon} alt="Logo da Avon" />
+        </div>
+        <div className="slide">
+          <img src={Luandre} alt="Logo da Luandre" />
+        </div>
+        <div className="slide">
+          <img src={Kopenhagen} alt="Logo da Kopenhagen" />
+        </div>
+        <div className="slide">
+          <img src={LaPercussi} alt="Logo da LaPercussi" />
+        </div>
+        <div className="slide">
+          <img src={BrasilCt} alt="Logo da BrasilCt" />
+        </div>
+        <div className="slide">
+          <img src={Goldratt} alt="Logo da Goldratt" />
+        </div>
+        <div className="slide">
+          <img src={Hcentrix} alt="Logo da Hcentrix" />
+        </div>
+        <div className="slide">
+          <img
+            className="machadoImg"
+            src={MachadoMeyer}
+            alt="Logo da Machado Meyer"
+          />
+        </div>
+        <div className="slide">
+          <img src={Modeat} alt="Logo da Modeat" />
+        </div>
+      </Slider>
+    </Container>
+  );
 }
 
 export default LandingPage;
--- src/components/pageCompnents/BrandsP/styles.js
+++ src/components/pageCompnents/BrandsP/styles.js
@@ -3,10 +3,36 @@ import styled from 'styled-components';
 export const Container = styled.div`
   min-height: 100vh;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: relative;
-  scroll-snap-align: start;
+  justify-content: flex-end;
+  flex-direction: column;
   scroll-snap-align: start;
   background-color: white;
+  /* overflow: visible;
+  overflow-x: hidden; */
+
+  p {
+    display: flex;
+    width: 70%;
+    align-self: flex-end;
+  }
+
+  .slideShow {
+    height: 300px;
+  }
+  .slide {
+    width: fit-content;
+    height: fit-content;
+    img {
+      width: 320px;
+      height: auto;
+    }
+    p {
+      color: white;
+    }
+  }
+
+  .machadoImg {
+    width: 320px;
+    height: 200px;
+  }
 `;
